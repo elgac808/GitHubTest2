b62a41210d7b3a76368aeda0f070d91da1e97a6a
elgac808 <JC60@students.ptcollege.edu>
2020-02-05 15:34:35 -0500

    Commit practice

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -26,5 +26,9 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo again");
     }
      
+     public static void Cagle(){
+         System.out.println("This is a test");
+     }
+     
      
 }
